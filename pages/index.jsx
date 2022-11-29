571967d0f58b9794519e7cb633b90a20ec8b9665
Menura Adithya <64089619+emiz98@users.noreply.github.com>
2022-11-29 15:11:17 +0530

    Cleaned up the starter files

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Google Play</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </div>
+  );
+};
+
+export default Home;
